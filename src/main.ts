50d237a5dfb8ac22d980bc3135b5a6e3caaf5544
Floris <florisjoepie@gmail.com>
2025-01-05 15:04:22 +0100

    base project setup with tailwind and gsap

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,28 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-const color = 0xFFFFFF;
-const intensity = 5;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(-1, 2, 10);
-scene.add(light);
-
-camera.position.z = 10;
+camera.position.z = 5;
 
 function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 }
