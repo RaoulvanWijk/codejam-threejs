9b7b1729e930f7bbfd4bde5095f94659e0780055
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2025-01-09 14:34:02 +0100

    finished the assignment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import gsap from "gsap";
 import * as THREE from "three";
+import { GLTFLoader, OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0.1,
+  1,
   1000
 );
 
@@ -13,16 +15,37 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new GLTFLoader();
+loader.load("./teacup_house/scene.gltf", function (gltf) {
+  scene.add(gltf.scene);
+  // document.addEventListener("scroll", () => {
+  //   scrollY = window.scrollY;
+  
+  //   // Use GSAP to animate the rotation based on scrollY
+  //   gsap.to(gltf.scene.rotation, {
+  //       x: scrollY * 0.01,  // Control the speed of rotation on the X axis
+  //       y: scrollY * 0.01,  // Control the speed of rotation on the Y axis
+  //       duration: 0.5,       // Duration of the transition
+  //       ease: "power2.out",  // Ease out for smoothness
+  //   });
+  // })
+});
 
-camera.position.z = 5;
+// const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 ); 
+// const material = new THREE.MeshPhysicalMaterial(); 
+// const cube = new THREE.Mesh( geometry, material ); scene.add( cube );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 10 );
+scene.add( directionalLight );
+// scene.add(cube);
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.z = 4;
+controls.update()
+var scrollY = 0;
+
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // gltfssss.scene.rotation.x += 0.01;
+  // gltfssss.scene.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
